0fd719242a399857b137338f20d113580e15c97f
Michael Manto <michaelbmanto@gmail.com>
2017-11-29 13:18:53 +0000

    Initial Commit
    Original Project as it was

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+    public GameObject iceCreamScoop;
+
+
+    private float[] conePos = { -2.1f, 0, 2.1f };
+    private float[] levels = { -2.0f, -1.5f, -1.0f, -0.5f, 0.0f, 0.5f };
+    private int[,] coneLayout = new int[3,5];
+    private int[] scoopStackSize = new int[3];
+
+
+    // private IceCream[] ConeA;
+    // Use this for initialization
+    void Start () {
+
+        parseOrder(GetNextOrder());
+
+       // Debug.Log("Ready to show");
+        for (int c = 0; c<3; c++)
+        {
+            //Debug.Log("Cone " + c.ToString());
+            for (int s = 0; s< scoopStackSize[c]; s++)
+            {
+                GameObject scoop = Instantiate(iceCreamScoop, new Vector3(conePos[c], levels[s],-s), Quaternion.identity) as GameObject;  //create sorting layers (no need to because can just set Z when move)
+                scoop.GetComponent<IceCream>().setFlavour("F"+coneLayout[c,s]);
+               // Debug.Log(coneLayout[c, s]);
+            }
+         
+
+        }
+       
+      
+    
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void parseOrder(string order)
+    {
+       // Debug.Log("Parsing Order: " + order);
+        string[] coneDetails;
+        string[][] scoopDetails = new string[3][];
+
+
+        coneDetails = order.Split(';');
+        for (int i = 0; i < 3; i++)
+        {
+            scoopDetails[i] = coneDetails[i].Split(',');  
+        }
+
+        for (int c =0; c<coneDetails.Length; c++)
+        {
+           //  Debug.Log("Cone " + c.ToString() + " has " + scoopDetails[c].Length);
+            scoopStackSize[c] = scoopDetails[c].Length;
+            for (int s = 0; s < scoopDetails[c].Length; s++)
+            {
+               // Debug.Log(scoopDetails[c][s]);
+                coneLayout[c,s] = int.Parse(scoopDetails[c][s]);
+            }
+            
+        }
+
+
+    }
+
+
+    string GetNextOrder()
+    {
+       // Debug.Log("Getting next order");
+        return "1,1,2;3,2;1,2,4";  // deal with empty
+    }
+}
---
+++ Assets/Scripts/IceCream.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceCream : MonoBehaviour {
+
+    private  SpriteRenderer flavourSprite;
+    public Sprite F1;
+    public Sprite F2;
+    public Sprite F3;
+    public Sprite F4;
+    public Sprite F5;
+
+    public string flavour;
+
+
+
+    // Use this for initialization
+    void Awake () {
+
+        flavourSprite = GetComponent<SpriteRenderer>();
+        flavour = "F1";
+        flavourSprite.sprite = F1;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setFlavour(string newFlavour)
+    {
+     
+        flavour = newFlavour;
+
+        switch(flavour)
+        {
+            case "F1": flavourSprite.sprite = F1; break;
+            case "F2": flavourSprite.sprite = F2; break;
+            case "F3": flavourSprite.sprite = F3; break;
+            case "F4": flavourSprite.sprite = F4; break;
+            case "F5": flavourSprite.sprite = F5; break;
+
+
+            default: flavourSprite.sprite = F1; break;
+
+        }
+     
+    }
+}
