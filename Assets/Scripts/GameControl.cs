4cf28d4845613a15dbc2de473e3ddc652158e79a
Michael Manto <michaelbmanto@gmail.com>
2017-12-07 16:11:15 +0000

    Display order created (visual only)
    the game now displays the layout of the current game and the order based on a function where you pass in locations and scales

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -9,8 +9,16 @@ public class GameControl : MonoBehaviour {
 
 
 
-    private float[] conePos = { -2.1f, 0, 2.1f };
+    private float[] conePos = { -2.1f, 0, 2.1f, -3.61f };
     private float[] levels = { -2.0f, -1.5f, -1.0f, -0.5f, 0.0f, 0.5f };
+
+    private float[] conePos_orders = { -1.33f, 0, 1.33f, 2.65f};
+    private float[] levels_orders = { 3.69f, 3.99f, 4.29f, 4.59f, 4.89f, 5.39f };
+    private float orderScale_cone = 0.65f;
+    private float orderScale_scoop = 0.65f;
+   
+
+
     private int[,] coneLayoutData = new int[3,5];
     private int[] scoopStackSize = new int[3];
     private Stack<GameObject>[] coneLayout = new Stack<GameObject>[3]; 
@@ -30,9 +38,15 @@ public class GameControl : MonoBehaviour {
         // Debug.Log("Ready to show");
 
         //Load the Ice Creams
+        DisplayIceCreams(conePos, levels, coneLayout, 1, 1);
+
+        //Displayt the order
+        DisplayIceCreams(conePos_orders, levels_orders, coneLayout, orderScale_scoop, orderScale_cone);
+
+        /*
         for (int c = 0; c<3; c++)
         {
-            GameObject cone = Instantiate(iceCreamCone, new Vector3(conePos[c], -3.61f, 1), Quaternion.identity) as GameObject;
+            GameObject cone = Instantiate(iceCreamCone, new Vector3(conePos[c], conePos[3], 1), Quaternion.identity) as GameObject;
 
             //Debug.Log("Cone " + c.ToString());
             for (int s = 0; s< scoopStackSize[c]; s++)
@@ -54,13 +68,40 @@ public class GameControl : MonoBehaviour {
             }
          
 
-        }   
+        } 
+        */
 
     }
 
-    void DisplayIceCreams(float[] conePos, float[] levels, Stack<GameObject>[] layout)
+    void DisplayIceCreams(float[] conePos, float[] levels, Stack<GameObject>[] layout, float scoopScale, float coneScale)
     {
+        for (int c = 0; c < 3; c++)
+        {
+            GameObject cone = Instantiate(iceCreamCone, new Vector3(conePos[c], conePos[3], 1), Quaternion.identity) as GameObject;
+            cone.transform.localScale = new Vector2(cone.transform.localScale.x * coneScale, cone.transform.localScale.y * coneScale);
 
+            //Debug.Log("Cone " + c.ToString());
+            for (int s = 0; s < scoopStackSize[c]; s++)
+            {
+                GameObject scoop = Instantiate(iceCreamScoop, new Vector3(conePos[c], levels[s], -s), Quaternion.identity) as GameObject;
+                scoop.GetComponent<IceCream>().setFlavour("F" + coneLayoutData[c, s]);
+                scoop.transform.localScale = new Vector2(scoop.transform.localScale.x*scoopScale, scoop.transform.localScale.y * scoopScale);
+                coneLayout[s].Push(scoop);
+
+                //For the top scoop
+                if (s == scoopStackSize[c] - 1)
+                {
+                    scoop.GetComponent<IceCream>().isTop = true;
+                }
+                //Otherwise
+                else
+                {
+                    scoop.GetComponent<IceCream>().isTop = false;
+                }
+            }
+
+
+        }
     }
 
 
